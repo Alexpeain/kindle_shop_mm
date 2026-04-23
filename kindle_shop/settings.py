313d4cdd62864b9fc5b32310c06bef7537aeb099
Alex Peain <tonghsengsom@gmail.com>
2026-04-23 22:33:10 +0630

    Fix image loading issues: update MEDIA_ROOT, fix template fields, add Swiper JS

--- kindle_shop/settings.py
+++ kindle_shop/settings.py
@@ -137,5 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'product_images'
 
